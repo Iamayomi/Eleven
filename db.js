02febe7d736edda57f45b16df53b5f88f227d3b9
Amodu Ayomide <77242644+Iamayomi@users.noreply.github.com>
2024-07-10 01:07:01 +0100

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: './config.env' });
 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.PG_URL, {
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
