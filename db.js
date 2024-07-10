e017330dd8389d301cdea69a4c8feddfbd853d16
Amodu Ayomide <77242644+Iamayomi@users.noreply.github.com>
2024-07-10 01:04:09 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: './config.env' });
 
-const sequelize = new Sequelize(process.env.POSTGRES_URL);
 
-
-
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+    }
+  }
+});
 
 const connectDatabase = async () => {
    console.log('checking database connection 🔃🔃🔃🔃');
